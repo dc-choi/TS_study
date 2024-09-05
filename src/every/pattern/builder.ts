3b6b5f974209578eb884d0850d1cf304f8b271c5
dc-choi <ddagae0805@gmail.com>
2024-09-05 20:53:23 +0900

    feat: buider 데코레이터로 구현

--- src/every/pattern/builder.ts
+++ src/every/pattern/builder.ts
@@ -1,87 +1,100 @@
-// @Builder
-export class NewUser {
-    private _name?: string;
-    private _email?: string;
-    private _password?: string;
-
-    constructor(name?: string, email?: string, password?: string) {
-        this._name = name;
-        this._email = email;
-        this._password = password;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get email() {
-        return this._email;
-    }
-
-    get password() {
-        return this._password;
-    }
-
-    static Builder = class {
-        private _name?: string;
-        private _email?: string;
-        private _password?: string;
-
-        name(name: string) {
-            this._name = name;
-            return this;
-        }
-
-        email(email: string) {
-            this._email = email;
-            return this;
-        }
-
-        password(password: string) {
-            this._password = password;
-            return this;
-        }
-
-        build() {
-            return new NewUser(this._name, this._email, this._password);
+abstract class BuilderInit {
+    static Builder = class<T extends object> {
+        [props: string]: any;
+        build(): Partial<T> {
+            return {} as Partial<T>;
         }
     };
 }
 
-// function Builder<T extends { new (...args: any[]): any }>(constructor: T) {
-//     const _ = new constructor();
-//     const keys = Object.keys(_);
+@Builder
+export class NewUser extends BuilderInit {
+    private name?: string;
+    private email?: string;
+    private password?: string;
 
-//     const temp = class extends constructor {
-//         static Builder = class {
-//             constructor() {
-//                 for (const key of keys) {
-//                     Object.defineProperty(this, key, {
-//                         ["_" + key]: {
-//                             value: null,
-//                             enumerable: true,
-//                             writable: true,
-//                             configurable: true,
-//                         },
-//                         [key]: {
-//                             value: function (value: any): typeof Builder {
-//                                 this["_" + key] = value;
-//                                 return this;
-//                             },
-//                             enumerable: false,
-//                             writable: false,
-//                             configurable: false,
-//                         },
-//                     });
-//                 }
-//             }
-//             build() {
-//                 return new constructor(...keys.map((key) => this["_" + key]));
-//             }
-//         };
-//     };
-//     temp.prototype = Object.create(constructor.prototype);
-//     temp.prototype.constructor = constructor;
+    constructor(name?: string, email?: string, password?: string) {
+        super();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
-//     return temp;
-// }
+    getName() {
+        return this.name;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    /**
+     * 기존 빌더 패턴 구현
+     */
+    // static Builder = class {
+    //     private _name?: string;
+    //     private _email?: string;
+    //     private _password?: string;
+    //
+    //     name(name: string) {
+    //         this._name = name;
+    //         return this;
+    //     }
+    //
+    //     email(email: string) {
+    //         this._email = email;
+    //         return this;
+    //     }
+    //
+    //     password(password: string) {
+    //         this._password = password;
+    //         return this;
+    //     }
+    //
+    //     build() {
+    //         return new NewUser(this._name, this._email, this._password);
+    //     }
+    // };
+}
+
+function Builder<T extends { new (...args: any[]): any }>(constructor: T) {
+    const _ = new constructor();
+    const keys = Object.keys(_);
+
+    const temp = class extends constructor {
+        static Builder = class {
+            private values: { [key: string]: any } = {};
+
+            constructor() {
+                for (const key of keys) {
+                    Object.defineProperty(this, key, {
+                        get: function (this: any) {
+                            return this.values[key];
+                        },
+                        set: function (this: any, value: any) {
+                            this.values[key] = value;
+                        },
+                        enumerable: true,
+                        configurable: true,
+                    });
+
+                    (this as any)[key] = function (value: any) {
+                        this.values[key] = value;
+                        return this;
+                    };
+                }
+            }
+            build() {
+                return new constructor(...keys.map((key) => this.values[key]));
+            }
+        };
+    };
+    // temp.prototype = Object.create(constructor.prototype);
+    // temp.prototype.constructor = constructor;
+
+    return temp;
+}
